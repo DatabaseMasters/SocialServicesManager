e143206c28b61dee6da0be4dc9566fcf35eea7e8
Milena Sapunova <milena.sapunova@gmail.com>
2017-06-10 12:01:20 +0300

    Add dataparser

---
+++ SocialServicesManager/SocialServicesManager.Data/DataParser/DataParser.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ProcessJSON.Core
+{
+    public static class DataParser
+    {
+        public static T ParseJson<T>(string filePath)
+        {
+            string text = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<T>(text);
+        }
+
+        public static T ParseXML<T>(string filePath, string root)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute();
+            xmlRoot.ElementName = root;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T), xmlRoot);
+
+            XmlReader reader = XmlReader.Create(filePath);
+
+            return (T)serializer.Deserialize(reader);
+        }
+    }
+}
